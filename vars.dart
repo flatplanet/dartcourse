c2d46bcb56d141d23a51e301986337e33bdc0038
John Elder <johne4196@gmail.com>
2022-04-27 08:52:57 -0700

    variables

---
+++ vars.dart
@@ -0,0 +1,28 @@
+void main() {
+	// Variables
+	// $ _ 
+	// Use CamelCase  firstName
+
+	var firstName = "John";
+	String lastName = "Elder";
+	int x = 41;
+
+	// Dynamic Variables
+	dynamic name = "John";
+
+	// Declare but not assign
+	var fullName;
+
+	print(fullName);
+
+	fullName = "John Elder";
+	print(fullName);
+
+	// Consts and Finals
+	const String myName = "Tim";
+	final String otherName = "Bill";
+
+	print(otherName);
+
+
+}
